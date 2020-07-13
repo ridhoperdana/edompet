4415fb651a7bd3e1582b9631fad5eb10e60a67ea
ridhoperdana <ridhoperdana@ymail.com>
2020-07-13 07:49:50 +0700

    feat: add setter map and get map on transaction and wallet

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -4,10 +4,35 @@ class Transaction {
   String type = '';
   int moneySpent = 0;
   DateTime dateTime;
+  String id;
 
   Transaction(this.type);
 
-  save() {
-    print('saving transaction');
+  Transaction.fromMap(Map<String, dynamic> map) {
+    this.shortDescription = map["short_desription"];
+    this.category = map["category"];
+    this.type = map["type"];
+    this.moneySpent = map["spent_value"];
+    this.dateTime = map["created_time"];
+    this.id = map["id"];
+  }
+
+  set setShortDescription(String value) => shortDescription;
+  set setCategory(String category) => category;
+  set setType(String value) => type;
+  set setMoneySpent(int value) => moneySpent;
+  set setCreatedTime(DateTime value) => dateTime;
+  set setId(String value) => id;
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map["id"] = this.id;
+    map["short_description"] = this.shortDescription;
+    map["category"] = this.category;
+    map["type"] = this.type;
+    map["spent_value"] = this.moneySpent;
+    map["created_time"] = this.dateTime;
+
+    return map;
   }
 }
--- lib/models/wallet.dart
+++ lib/models/wallet.dart
@@ -2,6 +2,29 @@ class Wallet {
   String name;
   int initialMoney;
   String color;
+  String id;
 
   Wallet(this.name, this.initialMoney, this.color);
+
+  Wallet.fromMap(Map<String, dynamic> map) {
+    this.id = map["id"];
+    this.initialMoney = map["initial_money"];
+    this.color = map["color"];
+    this.name = map["name"];
+  }
+
+  set setName(String value) => name;
+  set setInitialMoney(int value) => initialMoney;
+  set setColor(String value) => color;
+  set setId(String value) => id;
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map["id"] = this.id;
+    map["name"] = this.name;
+    map["initial_money"] = this.initialMoney;
+    map["color"] = this.color;
+
+    return map;
+  }
 }
